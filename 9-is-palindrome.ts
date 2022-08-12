721b5f4dee9efc29d9e435528c4d2f87bcdc9b70
symphony <jciurc@gmail.com>
2022-08-11 19:27:47 -0700

    add kata 9

---
+++ 9-is-palindrome.ts
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+ var isPalindrome = function(x) {
+  const string = x.toString();
+  const len = string.length();
+  const range = len / 2;
+
+  for (let i = 0; i < range; i++) {
+      if (string.)
+  }
+};
