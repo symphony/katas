79ca6b1be2a5e5a35fceb344427c3135a95ef8ac
symphony <jciurc@gmail.com>
2022-08-11 18:25:55 -0700

    working twoSum solution

---
+++ two-sum.ts
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  const arr = [...nums];
+  arr.sort((a, b) => a - b);
+
+  let inA = 0;
+  let inB = nums.length - 1;
+
+
+  for (let i = 0; i < nums.length; i++) {
+    const valA = arr[inA];
+    const valB = arr[inB];
+    const current = valA + valB;
+
+    if (current === target) {
+      return [nums.findIndex(v => v === valA), nums.findIndex(v => v === valB)];
+    }
+
+    if (current < target) {
+      inA++;
+    } else {
+      inB--;
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
