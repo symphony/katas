090699e0c8e54716963ccb511d6b23bd73c291de
symphony <jciurc@gmail.com>
2022-08-11 19:33:11 -0700

    complete 9 is palindrome

--- 9-is-palindrome.ts
+++ 9-is-palindrome.ts
@@ -2,12 +2,17 @@
  * @param {number} x
  * @return {boolean}
  */
- var isPalindrome = function(x) {
+var isPalindrome = function (x) {
   const string = x.toString();
-  const len = string.length();
-  const range = len / 2;
+  const range = string.length / 2;
 
   for (let i = 0; i < range; i++) {
-      if (string.)
+    if (string.charAt(i) !== string.charAt(string.length - 1 - i)) return false;
   }
+
+  return true;
 };
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(10));
