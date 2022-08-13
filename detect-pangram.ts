6152664cf47df46f5a2b27b91f8f75c73f0fffbf
jordan <jciurc@gmail.com>
2022-08-13 16:38:23 -0700

    Update detect-pangram.ts
    use regex and sets

--- detect-pangram.ts
+++ detect-pangram.ts
@@ -1,9 +1,7 @@
-const isPangram = (string) => {
-  const phrase = string.toLowerCase();
-  return 'abcdefghijklmnopqrstuvwxyz'
-    .split('')
-    .every((a) => phrase.includes(a));
-};
+const isPangram = (string) => (
+  new Set(string.toLowerCase().replace(/[^a-z]+/g, '').split('')
+  ).size === 26
+);
 
 const string1 = "The quick brown fox jumps over the lazy dog."
 const string2 = "This is not a pangram."
