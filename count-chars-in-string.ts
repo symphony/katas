e381c86d82612718bfeae8bd4df796c013d9cf46
jordan <jciurc@gmail.com>
2022-08-13 15:54:35 -0700

    Update count-chars-in-string.ts

--- count-chars-in-string.ts
+++ count-chars-in-string.ts
@@ -1,6 +1,6 @@
-function count (string) {  
-  return string.split('').reduce((total, c) => {
+const count = (string) => (  
+  string.split('').reduce((total, c) => {
     total[c] = (total[c] ?? 0) + 1;
     return total;
   }, {})
-};
+);
