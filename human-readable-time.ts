9e5f7b316a86dee0a3e308ec487f5d225a864449
symphony <jciurc@gmail.com>
2022-08-15 13:21:09 -0700

    minify human readable further

--- human-readable-time.ts
+++ human-readable-time.ts
@@ -1,2 +1,1 @@
-const p = (n) => (n < 10 ? '0' : '') + (n | 0);
-const humanReadable = (s) => `${p(s / 3600)}:${p(s / 60 % 60)}:${p(s % 60)}`;
+const humanReadable = s => [s / 3600, s / 60 % 60, s % 60].map(n => (n < 10 ? '0' : '') + (n | 0)).join(':');
