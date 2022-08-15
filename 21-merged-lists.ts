fd552ccd258c0c25e166e45a8f59722e123c3d06
symphony <jciurc@gmail.com>
2022-08-15 12:42:22 -0700

    clean up merged-lists

--- 21-merged-lists.ts
+++ 21-merged-lists.ts
@@ -10,24 +10,6 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-// const sortLists = (node1, node2) => {
-//   if (node1.next === null) return node1.next = node2;
-//   const [first, second] = [node1, node2].sort((a, b) => a.next.val - b.val);
-//   const rest = second.next;
-//   second.next = first.next;
-//   first.next = second;
-//   if (rest) sortLists(first.next, rest);
-// };
-
-// var mergeTwoLists = function (list1, list2) {
-//   if (!list1 || !list2) return list1 || list2 || null;
-
-//   const [first, second] = [list1, list2].sort((a, b) => a.val - b.val);
-
-//   sortLists(first, second);
-//   return first;
-// };
-
 const mergeTwoLists = (list1, list2) => {
   if (list1 === null && list2 === null) return null;
   if (list1 === null) return list2;
