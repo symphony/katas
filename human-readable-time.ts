3b5f385c6102acf92dec1e5f1c710c8988010be2
symphony <jciurc@gmail.com>
2022-08-15 13:20:53 -0700

    minify human readable

--- human-readable-time.ts
+++ human-readable-time.ts
@@ -1,23 +1,2 @@
-const pad = (num) => (num < 10 ? '0' : '') + (num | 0);
-const humanReadable = (seconds) => [
-  pad(seconds / 3600),
-  pad(seconds / 60 % 60),
-  pad(seconds % 60),
-].join(':');
-
-// const strictEqual = require("chai").assert.strictEqual;
-
-// describe('tests', function() {
-//   it('should format correctly', function() {
-//     strictEqual(humanReadable(0), '00:00:00', 'humanReadable(0)');
-//     strictEqual(humanReadable(59), '00:00:59', 'humanReadable(59)');
-//     strictEqual(humanReadable(60), '00:01:00', 'humanReadable(60)');
-//     strictEqual(humanReadable(90), '00:01:30', 'humanReadable(90)');
-//     strictEqual(humanReadable(3599), '00:59:59', 'humanReadable(3599)');
-//     strictEqual(humanReadable(3600), '01:00:00', 'humanReadable(3600)');
-//     strictEqual(humanReadable(45296), '12:34:56', 'humanReadable(45296)');
-//     strictEqual(humanReadable(86399), '23:59:59', 'humanReadable(86399)');
-//     strictEqual(humanReadable(86400), '24:00:00', 'humanReadable(86400)');
-//     strictEqual(humanReadable(359999), '99:59:59', 'humanReadable(359999)');
-//   });
-// });
+const p = (n) => (n < 10 ? '0' : '') + (n | 0);
+const humanReadable = (s) => `${p(s / 3600)}:${p(s / 60 % 60)}:${p(s % 60)}`;
