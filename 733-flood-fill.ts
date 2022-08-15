2f92a9ce53d330df4cee2ff77b3c04de9523abdd
symphony <jciurc@gmail.com>
2022-08-15 12:07:49 -0700

    create 733-flood-fill

---
+++ 733-flood-fill.ts
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+const floodFill = (image, sr, sc, color) => {
+
+};
+
+// Examples
+// [[1,1,1],[1,1,0],[1,0,1]]
+// 1
+// 1
+// 2
+// [[0,0,0],[0,0,0]]
+// 0
+// 0
+// 0
