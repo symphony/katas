2d06cfb1fe5a91f5ec62826495259b55702f55b1
symphony <jciurc@gmail.com>
2022-08-15 15:57:18 -0700

    complete next smaller digits (unoptimal)

--- k4-next_smaller_digits.ts
+++ k4-next_smaller_digits.ts
@@ -1,9 +1,28 @@
+const permutator = (inputArr) => {
+  let result = [];
+
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next))
+      }
+    }
+  }
+  permute(inputArr)
+
+  return result;
+}
+
 const nextSmaller = (n) => {
   const split = n.toString().split('');
-  const variations = split.map((a, i) => (split.slice(i + 1)
-    .map((b) => a + b)
-  ).reduce((acc, val) => acc.concat(val), [])
-  ).sort((a, b) => a - b);
+  const variations = permutator(split)
+    .map((v) => Number(v.join('')))
+    .filter((v) => v.toString().length === split.length)
+    .sort((a, b) => a - b);
 
   console.log(variations);
 
