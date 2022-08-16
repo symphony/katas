99352b58e0231449c51035347426004a63de756f
symphony <jciurc@gmail.com>
2022-08-15 19:37:48 -0700

    refactor isbn validation

--- k6-isbn-validation.ts
+++ k6-isbn-validation.ts
@@ -1,13 +1,7 @@
 const validISBN10 = (isbn) => (
-  /^[0-9]{9}[0-9X]$/
-    .test(isbn)
-    ? isbn
-      .split('')
-      .reduce((val, n, i) => val + (
-        n === 'X'
-          ? 100
-          : Number(n) * (i + 1)
-      ), 0)
-    % 11 === 0
+  /^\d{9}[\dX]$/.test(isbn)
+    ? !([...isbn].reduce((sum, n, i) => sum + (
+      n === 'X' ? 100 : n * (i + 1)
+    ), 0) % 11)
     : false
 );
