a8d3c84caf48f6096e7b3206c18dd54359bb58f3
symphony <jciurc@gmail.com>
2022-08-15 17:53:44 -0700

    clean up smaller digits. remove unecessary type conversion

--- k4-next_smaller_digits.ts
+++ k4-next_smaller_digits.ts
@@ -1,22 +1,22 @@
 const nextSmaller = (n) => {
-  const split = n.toString().split('').map((c) => Number(c)).sort();
+  const sorted = n.toString().split('').sort();
 
   // fix leading 0s
-  if (split[0] === 0) {
-    const found = split.findIndex((n) => n > 0);
-    split[0] = split[found];
-    split[found] = 0;
+  if (sorted[0] === '0') {
+    const found = sorted.findIndex((n) => n !== '0');
+    sorted[0] = sorted[found];
+    sorted[found] = '0';
   }
 
-  const min = Number(split.join(''));
+  const min = Number(sorted.join(''));
   if (min >= n) return -1;
 
   for (let i = n - 9; i >= min; i--) {
-    const copy = n.toString().split('').sort().join();
-    const current = i.toString().split('').sort().join();
+    const copy = n.toString().split('').sort().join('');
+    const current = i.toString().split('').sort().join('');
 
     if (copy === current) return i;
   }
 
   return -1;
-};
+}
