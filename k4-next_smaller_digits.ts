8855fae8cefc470711fe5ff0a7f3c0243cdf3833
symphony <jciurc@gmail.com>
2022-08-15 17:42:34 -0700

    optimize next-smaller-digits with c style loop

--- k4-next_smaller_digits.ts
+++ k4-next_smaller_digits.ts
@@ -1,32 +1,22 @@
-// = helpers =
-const findCombinations = (arr) => {
-  const result = [];
-
-  // recursively build every combination
-  const permute = (list, m = []) => {
-    if (list.length === 0) {
-      result.push(m);
-      return;
-    }
-
-    for (let i = 0; i < list.length; i++) {
-      const curr = list.slice();
-      const next = curr.splice(i, 1);
-      permute(curr.slice(), m.concat(next))
-    }
-  };
-
-  permute(arr)
-  return result;
-};
-
-// = main function =
 const nextSmaller = (n) => {
-  const split = n.toString().split('');
-  const variations = findCombinations(split)
-    .map((v) => Number(v.join('')))
-    .filter((v) => v.toString().length === split.length) // filter leading 0s
-    .sort((a, b) => a - b);
+  const split = n.toString().split('').map((c) => Number(c)).sort();
 
-  return variations[variations.findIndex((v) => v === n) - 1] || -1;
+  // fix leading 0s
+  if (split[0] === 0) {
+    const found = split.findIndex((n) => n > 0);
+    split[0] = split[found];
+    split[found] = 0;
+  }
+
+  const min = Number(split.join(''));
+  if (min >= n) return -1;
+
+  for (let i = n - 9; i >= min; i--) {
+    const copy = n.toString().split('').sort().join();
+    const current = i.toString().split('').sort().join();
+
+    if (copy === current) return i;
+  }
+
+  return -1;
 };
