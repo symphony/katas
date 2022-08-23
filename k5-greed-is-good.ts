ad1c72adb7b753617aed88394cc9a4e520101824
symphony <jciurc@gmail.com>
2022-08-23 14:22:16 -0700

    create greed is good kata

---
+++ k5-greed-is-good.ts
@@ -0,0 +1,7 @@
+/**
+ * Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+ * @param dice
+ */
+function score(dice) {
+  // Fill me in!
+}
