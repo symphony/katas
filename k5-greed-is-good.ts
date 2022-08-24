8343b293e8a23e38acda7b215ab45dcd4bc7df37
symphony <jciurc@gmail.com>
2022-08-24 12:32:10 -0700

    complete greed is good

--- k5-greed-is-good.ts
+++ k5-greed-is-good.ts
@@ -1,7 +1,27 @@
 /**
  * Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+ * Three 1's => 1000 points
+ * Three 6's =>  600 points
+ * Three 5's =>  500 points
+ * Three 4's =>  400 points
+ * Three 3's =>  300 points
+ * Three 2's =>  200 points
+ * One   1   =>  100 points
+ * One   5   =>   50 point
  * @param dice
  */
-function score(dice) {
-  // Fill me in!
-}
+const score = (dice) => {
+  const count = {};
+  let score = 0;
+
+  for (const roll of dice) {
+    count[roll] = (count[roll] || 0) + 1;
+  }
+
+  score += Math.floor((count[1] || 0) / 3) * 1000 + ((count[1] || 0) % 3) * 100;
+  score += Math.floor((count[5] || 0) / 3) * 500 + ((count[5] || 0) % 3) * 50;
+  score += Math.floor((count[2] || 0) / 3) * 200;
+  score += Math.floor((count[3] || 0) / 3) * 300;
+  score += Math.floor((count[4] || 0) / 3) * 400;
+  return score;
+};
