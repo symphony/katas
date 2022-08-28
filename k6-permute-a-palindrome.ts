f18231555796a983545dbacbd0d662cd5c8f0f94
symphony <jciurc@gmail.com>
2022-08-27 18:53:51 -0700

    create permute a palindrome kata

---
+++ k6-permute-a-palindrome.ts
@@ -0,0 +1,8 @@
+/**
+* Write a function that will check whether ANY permutation of the characters of the input string is a palindrome. Bonus points for a solution that is efficient and/or that uses only built-in language functions. Deem yourself brilliant if you can come up with a version that does not use any function whatsoever.
+*/
+
+function permuteAPalindrome(input) {
+
+  return true;
+};
