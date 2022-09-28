8810f4e8cbef8370cc4062dbcff47694d8b13e61
symphony <jciurc@gmail.com>
2022-09-27 20:15:17 -0700

    formatting in js nesting structure

--- k4-nesting-structure-comparison.ts
+++ k4-nesting-structure-comparison.ts
@@ -4,9 +4,9 @@ const isSameStructure = (original, other) => {
   if (Array.isArray(original) && Array.isArray(other)) {
     for (let i = 0; i < original.length; i++) {
       if (!isSameStructure(original[i], other[i])) return false;
-    }
+    };
     return original.length === other.length;
-  }
+  };
 
   return false;
 };
