e63befe50d6cfc60833075a6507298edf5c8f1f7
symphony <jciurc@gmail.com>
2022-09-28 08:33:45 -0700

    udpate sum of intervals. adjust a value as they're added

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -2,24 +2,22 @@ const sumIntervals = (intervals) => {
   intervals.sort((a, b) => a[1] - b[1]);
   console.log(intervals);
 
-  let total = intervals[0][1] - intervals[0][0];
-  // let total = 0;
-  console.log(0, 0, 'adding', [intervals[0][1], intervals[0][0]], 'total:', total);
-  for (let i = 1; i < intervals.length; i++) {
+  // let total = intervals[0][1] - intervals[0][0];
+  let total = 0;
+  for (let i = 0; i < intervals.length; i++) {
     let [a, b] = intervals[i];
 
-    // for (let j = 0; j < i; j++) {
-    const [prevLow, prevHigh] = intervals[i - i];
-    if (a < prevLow) {
-      [a, b] = [Math.min(prevLow, a), Math.min(prevLow, b)]
-      total += b - a;
-      console.log(i, 0, 'low adding', [a, b], 'total:', total);
-      continue;
+    for (let j = 0; j < i; j++) {
+      const [prevLow = Infinity, prevHigh = Infinity] = intervals[i - i];
+      if (a < prevLow) {
+        total += Math.min(prevLow, b) - Math.min(prevLow, a);
+        a = Math.min(prevHigh, b);
+        continue;
+      };
+      [a, b] = [Math.max(prevHigh, a), Math.max(prevHigh, b)];
     };
-    [a, b] = [Math.max(prevHigh, a), Math.max(prevHigh, b)];
-    // };
     total += b - a;
-    console.log(i, 0, 'hih adding', [a, b], 'total:', total);
+    console.log(i, 0, 'adding', [a, b], 'total:', total);
   };
 
   return total;
