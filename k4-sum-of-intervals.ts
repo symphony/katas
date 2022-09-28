4ab5152d09870487d5c48c57b453f07f274c1685
symphony <jciurc@gmail.com>
2022-09-28 15:26:11 -0700

    pass happy path with new consolidate function

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -1,25 +1,25 @@
 const consolidate = (list) => {
-  const accumulator = [[0, 0]];
+  list.sort((a, b) => a[1] - b[1]);
+  console.log('before', list);
+
+  const accumulator = [list.shift()];
   for (const next of list) {
-    for (const prev of accumulator) {
-      // check if overlap previous range
+    for (const prev of [...accumulator]) {
+      // check if fully above or below previous range
+      if (next[1] < prev[0]) accumulator.unshift(next);
+      if (next[0] > prev[1]) accumulator.push(next);
+
+      // check for overlap and stretch previous start or end range
       if (next[0] < prev[0] && next[1] >= prev[0]) prev[0] = next[0];
       if (next[1] > prev[1] && next[0] <= prev[1]) prev[1] = next[1];
-
-      // check if fully above or below previous range
-      if (next[1] - 1 < prev[0]) accumulator.unshift(next);
-      if (next[0] - 1 > prev[1]) accumulator.push(next);
     };
   };
 
-  return accumulator;
+  console.log('after', accumulator);
+  // if lists are the same length it means no more consolidating can be done
+  return accumulator.length >= list.length ? accumulator : consolidate(accumulator);
 };
 
 const sumIntervals = (intervals) => {
-  intervals.sort((a, b) => a[1] - b[1]);
-  console.log('before', intervals);
-
-  const consolidated = consolidate(intervals);
-  console.log('after', consolidated);
-  return consolidated.reduce((prev, next) => prev += next[1] - next[0], 0);
+  return consolidate(intervals).reduce((prev, next) => prev += next[1] - next[0], 0);
 };
