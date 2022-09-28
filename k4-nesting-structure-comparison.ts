040a671e720b3ab990c5627425088db15ad62d14
symphony <jciurc@gmail.com>
2022-09-27 18:36:59 -0700

    create base case

--- k4-nesting-structure-comparison.ts
+++ k4-nesting-structure-comparison.ts
@@ -1,7 +1,5 @@
 Array.prototype.sameStructureAs = function (other) {
-  // Return 'true' if and only if 'other' has the same
-  // nesting structure as 'this'.
 
-  // Note: You are given a function isArray(o) that returns
-  // whether its argument is an array.
+
+  return this.length === other.length;
 };
