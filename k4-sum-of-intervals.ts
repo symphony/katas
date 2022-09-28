5abb2fb78f9da1236bed12323e1349176d2cc52b
symphony <jciurc@gmail.com>
2022-09-28 12:52:09 -0700

    create helper function in sum of intervals

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -1,3 +1,14 @@
+const consolidate = (prev, next) => {
+  const consolidated = [];
+  if (next[0] < prev[0]) {
+    total += Math.min(prev[0], next[1]) - next[0];
+    next[0] = Math.min(prev[1], next[1]);
+  };
+  if (next[0] < prev[1]) {
+    total += Math.max(prev[1], next[1]) - prev[1];
+  };
+};
+
 const sumIntervals = (intervals) => {
   intervals.sort((a, b) => a[1] - b[1]);
   console.log(intervals);
@@ -7,17 +18,21 @@ const sumIntervals = (intervals) => {
   for (let i = 0; i < intervals.length; i++) {
     let [a, b] = intervals[i];
 
-    for (let j = 0; j < i; j++) {
+    for (let j = 0; j <= i; j++) {
       const [prevLow = Infinity, prevHigh = Infinity] = intervals[i - i];
       if (a < prevLow) {
-        total += Math.min(prevLow, b) - Math.min(prevLow, a);
+        total += Math.min(prevLow, b) - a;
         a = Math.min(prevHigh, b);
         continue;
       };
-      [a, b] = [Math.max(prevHigh, a), Math.max(prevHigh, b)];
+      if (a < prevHigh) {
+        total += Math.max(prevHigh, b) - prevHigh;
+        continue;
+      };
+      total += b - a;
+      console.log(i, 0, 'adding', [a, b], 'total:', total);
     };
-    total += b - a;
-    console.log(i, 0, 'adding', [a, b], 'total:', total);
+
   };
 
   return total;
