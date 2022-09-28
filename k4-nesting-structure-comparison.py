37cbacc55b0aff34a8f9a943bc0022250b36a097
symphony <jciurc@gmail.com>
2022-09-27 19:20:33 -0700

    update nesting structure kata

--- k4-nesting-structure-comparison.py
+++ k4-nesting-structure-comparison.py
@@ -1,2 +1,8 @@
 def same_structure_as(original, other):
-    return len(original) == len(other)
+    if not isinstance(original, list) and not isinstance(other, list):
+        return True
+    elif isinstance(original, list) and isinstance(other, list):
+        for a, b in zip(original, other):
+            if not same_structure_as(a, b):
+                return False
+    return False
