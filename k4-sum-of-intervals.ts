94eaed9ff870463020f8e9a454c7cf59c0bbcb18
symphony <jciurc@gmail.com>
2022-09-27 20:30:11 -0700

    create bad quadratic solution for sum of intervals

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -1,3 +1,12 @@
 const sumIntervals = (intervals) => {
-  //TODO
-}
+  const mySet = new Set();
+
+  for (const range of intervals) {
+    for (let i = range[0]; i < range[1]; i++) {
+      mySet.add(i)
+    };
+  }
+
+
+  return [...mySet].length;
+};
