97be3c34986586d95b4334b564a257fc64a89e82
symphony <jciurc@gmail.com>
2022-09-28 00:11:51 -0700

    stuck on sum of intervals

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -2,18 +2,24 @@ const sumIntervals = (intervals) => {
   intervals.sort((a, b) => a[1] - b[1]);
   console.log(intervals);
 
-  // let total = intervals[0][1] - intervals[0][0];
-  let total = 0;
-  for (let i = 0; i < intervals.length; i++) {
+  let total = intervals[0][1] - intervals[0][0];
+  // let total = 0;
+  console.log(0, 0, 'adding', [intervals[0][1], intervals[0][0]], 'total:', total);
+  for (let i = 1; i < intervals.length; i++) {
     let [a, b] = intervals[i];
 
-    for (let j = 0; j < i; j++) {
-      const [prevLow, prevHigh] = intervals[j];
-      a = a < prevLow ? Math.min(prevLow, a) : Math.max(prevHigh, a);
-      b = a < prevLow ? Math.min(prevLow, b) : Math.max(prevHigh, b);
+    // for (let j = 0; j < i; j++) {
+    const [prevLow, prevHigh] = intervals[i - i];
+    if (a < prevLow) {
+      [a, b] = [Math.min(prevLow, a), Math.min(prevLow, b)]
       total += b - a;
+      console.log(i, 0, 'low adding', [a, b], 'total:', total);
+      continue;
     };
-    console.log(i, 'adding', [a, b], 'total:', total);
+    [a, b] = [Math.max(prevHigh, a), Math.max(prevHigh, b)];
+    // };
+    total += b - a;
+    console.log(i, 0, 'hih adding', [a, b], 'total:', total);
   };
 
   return total;
