ddb56bf053faaec2aa38b6c753ad785bdb75f98d
symphony <jciurc@gmail.com>
2022-09-27 19:53:12 -0700

    create js version of nesting structure comparison

---
+++ k4-nesting-structure-comparison.ts
@@ -0,0 +1,10 @@
+def same_structure_as(original, other):
+    if not isinstance(original, list) and not isinstance(other, list):
+        return True
+    elif isinstance(original, list) and isinstance(other, list):
+        for a, b in zip(original, other):
+            if not same_structure_as(a, b):
+                return False
+        return len(original) == len(other)
+    # if one but not both arguments are a list
+    return False
