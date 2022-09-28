f9db473694f1bc4faf7a00d6bfff653f4efa1b66
symphony <jciurc@gmail.com>
2022-09-27 23:49:28 -0700

    updated sum of intervals

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -2,21 +2,18 @@ const sumIntervals = (intervals) => {
   intervals.sort((a, b) => a[1] - b[1]);
   console.log(intervals);
 
+  // let total = intervals[0][1] - intervals[0][0];
   let total = 0;
   for (let i = 0; i < intervals.length; i++) {
     let [a, b] = intervals[i];
 
     for (let j = 0; j < i; j++) {
       const [prevLow, prevHigh] = intervals[j];
-      if (a >= prevLow && a < prevHigh) {
-        a = prevHigh;
-        b = Math.max(prevHigh, b);
-      };
+      a = a < prevLow ? Math.min(prevLow, a) : Math.max(prevHigh, a);
+      b = a < prevLow ? Math.min(prevLow, b) : Math.max(prevHigh, b);
+      total += b - a;
     };
-
-    total += b - a;
-    console.log('adding', [a, b], 'total:', total);
-    console.log('total', total);
+    console.log(i, 'adding', [a, b], 'total:', total);
   };
 
   return total;
