7fc816dfce912aeb871ad0a45a8650b553fb5489
symphony <jciurc@gmail.com>
2022-09-27 22:11:45 -0700

    go back to non overlapping working

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -1,17 +1,10 @@
 const sumIntervals = (intervals) => {
-  console.log('checking', intervals);
+  console.log('adding', intervals);
   let total = 0;
 
   for (const range of intervals) {
-    const highests = [].concat(...intervals.map((current) => range !== current ? current[1] : []))
-    console.log('found', highests);
-    const highest = highests
-      .sort((a, b) => b - a)
-      .find((n) => range[1] > n && n >= range[0])
-      || range[0];
-    console.log('highest', highest);
-
-    total += range[1] - highest;
+    console.log('current', range);
+    total += range[1] - range[0];
   };
 
   return total;
