bf95ad1fe46d467ec144aebb25c3cc5a7d89732c
symphony <jciurc@gmail.com>
2022-09-27 18:29:05 -0700

    create nesting structure comparison kata

---
+++ k4-nesting-structure-comparison.ts
@@ -0,0 +1,7 @@
+Array.prototype.sameStructureAs = function (other) {
+  // Return 'true' if and only if 'other' has the same
+  // nesting structure as 'this'.
+
+  // Note: You are given a function isArray(o) that returns
+  // whether its argument is an array.
+};
