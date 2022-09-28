3c62297879e6c8ef4c91710a371104848dff7bf6
symphony <jciurc@gmail.com>
2022-09-28 16:04:59 -0700

    cleanup consolidate feature. get checks working. need to check whole array if matching, otherwise continue

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -1,25 +1,26 @@
 const consolidate = (list) => {
-  list.sort((a, b) => a[1] - b[1]);
-  console.log('before', list);
-
   const accumulator = [list.shift()];
-  for (const next of list) {
-    for (const prev of [...accumulator]) {
-      // check if fully above or below previous range
-      if (next[1] < prev[0]) accumulator.unshift(next);
-      if (next[0] > prev[1]) accumulator.push(next);
 
+  for (const next of list) {
+    let overlap = false;
+    for (const prev of [...accumulator]) {
+      console.log('current', accumulator);
+      // check if fully above or below previous range
+      if (next[1] < prev[0] || next[0] > prev[1]) continue;
+
+      overlap = true;
       // check for overlap and stretch previous start or end range
-      if (next[0] < prev[0] && next[1] >= prev[0]) prev[0] = next[0];
-      if (next[1] > prev[1] && next[0] <= prev[1]) prev[1] = next[1];
+      if (next[0] < prev[0] && next[1] >= prev[0]) prev[0] = next[1] = next[0];
+      if (next[1] > prev[1] && next[0] <= prev[1]) prev[1] = next[0] = next[1];
     };
+    if (!overlap) accumulator.push(next);
   };
 
-  console.log('after', accumulator);
   // if lists are the same length it means no more consolidating can be done
-  return accumulator.length >= list.length ? accumulator : consolidate(accumulator);
+  return accumulator.length < list.length ? consolidate(accumulator) : accumulator;
 };
 
 const sumIntervals = (intervals) => {
+  console.log('reducing', intervals);
   return consolidate(intervals).reduce((prev, next) => prev += next[1] - next[0], 0);
 };
