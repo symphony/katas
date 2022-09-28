25fdd1ded191a392dedb292f9bc7456edfa7c0bd
symphony <jciurc@gmail.com>
2022-09-27 21:55:02 -0700

    get one test passing

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -1,9 +1,17 @@
 const sumIntervals = (intervals) => {
+  console.log('checking', intervals);
   let total = 0;
 
   for (const range of intervals) {
-    console.log(range);
-    total += range[1] - range[0];
+    const highests = [].concat(...intervals.map((current) => range !== current ? current[1] : []))
+    console.log('found', highests);
+    const highest = highests
+      .sort((a, b) => b - a)
+      .find((n) => range[1] > n && n >= range[0])
+      || range[0];
+    console.log('highest', highest);
+
+    total += range[1] - highest;
   };
 
   return total;
