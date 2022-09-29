7f5765b28703713b8df4a8fb963fb6cb540f2212
symphony <jciurc@gmail.com>
2022-09-28 17:10:08 -0700

    solve happy path for reduce variation

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -2,8 +2,8 @@ const sumIntervals = (intervals) => {
   const offset = intervals.sort((a, b) => a[0] - b[0])[0][0];
   const zeroAdjust = intervals.map((val) => [val[0] - offset, val[1] - offset]);
 
-  const squashed = zeroAdjust.reduce((prev, val) => {
+  return zeroAdjust.reduce((prev, val) => {
     if (val[0] <= prev) return val[1];
-
+    return val[1] - val[0] - (val[0] - prev);
   }, 0)
 };
