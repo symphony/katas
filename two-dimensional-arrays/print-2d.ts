f19ad5960ab9ee46602448dd9b1ef46b0851718d
symphony <jciurc@gmail.com>
2022-09-29 09:04:23 -0700

    create 2d array exercise files

---
+++ two-dimensional-arrays/print-2d.ts
@@ -0,0 +1,36 @@
+// Write a function `print2d` that accepts a two-dimensional array as an argument. The function
+// should print all inner elements of the array.
+
+const array1 = [
+  ["a", "b", "c", "d"],
+  ["e", "f"],
+  ["g", "h", "i"],
+];
+const array2 = [[9, 3, 4], [11], [42, 100]];
+
+const print2d = (arr) => {
+
+};
+
+
+print2d(array1);
+// prints
+//  a
+//  b
+//  c
+//  d
+//  e
+//  f
+//  g
+//  h
+//  i
+
+
+print2d(array2);
+// prints
+//  9
+//  3
+//  4
+//  11
+//  42
+//  100
