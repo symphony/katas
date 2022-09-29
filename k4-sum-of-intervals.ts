cbea5b81847c3fcb7c6af10b2b2e131d858020bf
symphony <jciurc@gmail.com>
2022-09-28 17:34:57 -0700

    solve happy path for sorted method

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -3,7 +3,18 @@ const sumIntervals = (intervals) => {
   const sorted = intervals.sort((a, b) => a[0] - b[1]);
   console.log('sorted', sorted);
 
-  return sorted.reduce((sum, val) => {
-    return val[1] - val[0] - (val[0] - sum);
-  }, 0)
+  let sum = 0;
+  let prevHigh = sorted[0][0];
+
+  for (const [nextLow, nextHigh] of sorted) {
+    const low = Math.max(prevHigh, nextLow);
+    const high = Math.max(prevHigh, nextHigh);
+
+    console.log('prev', prevHigh, 'low', low, 'high', high);
+    sum += high - low;
+    prevHigh = high;
+    console.log('sum', sum);
+  };
+
+  return sum;
 };
