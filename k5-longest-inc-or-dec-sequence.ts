24fd71b2622e4a2ca434b14991b997627a6ee644
symphony <jciurc@gmail.com>
2022-09-29 16:59:04 -0700

    refactor LIS push to splice

--- k5-longest-inc-or-dec-sequence.ts
+++ k5-longest-inc-or-dec-sequence.ts
@@ -1,24 +1,22 @@
 const longestComb = (arr, command) => {
+  console.log(arr);
   const decreasing = command === '> >' || command === '>>';
-  const validSequences = arr.map(() => []);
-  const counts = Array(arr.length).fill(1);
+  const validSequences = arr.map((num) => [num]);
+  const counts = Array(arr.length).fill(Array(arr.length).fill(1));
 
   for (let i = 1; i < arr.length; i++) {
     for (let j = 0; j < i; j++) {
       if (arr[decreasing ? i : j] < arr[decreasing ? j : i]) {
-        const newLen = counts[j] + 1;
-        if (newLen > counts[i]) {
-          counts[i] = newLen;
-          validSequences[i].push(arr[j]);
+        const newLen = counts[j][j] + 1;
+        if (newLen > counts[j][i]) {
+          counts[j][i] = newLen;
+          validSequences[i].splice(-1, 0, arr[j]);
         };
       };
     };
-
-    // add end node value to sequence
-    validSequences[i].push(arr[i]);
   };
-  console.log(counts);
-  console.log(validSequences);
+  decreasing && console.log(counts);
+  decreasing && console.log('results', validSequences);
 
   const highest = Math.max(...validSequences.map((list) => list.length));
   if (highest < 3) return [];
