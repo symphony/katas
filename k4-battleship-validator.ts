f3ebbeca9011f938ca082b9c983f8e08c8bddab8
symphony <jciurc@gmail.com>
2022-09-29 08:49:49 -0700

    create battleship validator kata

---
+++ k4-battleship-validator.ts
@@ -0,0 +1,15 @@
+function validateBattlefield(field) {
+  const sizes = {
+    battleships: 4,
+    cruisers: 3,
+    destroyers: 2,
+    submarines: 1,
+  };
+
+  const shipCounts = {
+    battleship: 4,
+    cruiser: 2,
+    destroyer: 2,
+    submarine: 4,
+  };
+};
