b27086db3aec3939d0a720c3d32be1876d881ef0
symphony <jciurc@gmail.com>
2022-09-29 15:26:57 -0700

    refactor longest inc/dec sequence to use helper for decreasing

--- k5-longest-inc-or-dec-sequence.ts
+++ k5-longest-inc-or-dec-sequence.ts
@@ -1,19 +1,34 @@
 const longestComb = (arr, command) => {
-  if (command === '< <') arr.reverse();
-  const lengths = Array(arr.length).fill(1);
-  // initialize all valid sequences to include each end position
-  const validSequences = arr.map((num) => [num]);
+  const decreasing = command === '> >' || command === '>>';
+  const validSequences = arr.map(() => []);
+  const counts = Array(arr.length).fill(1);
+
+  const pushOrUnshift = (array, element, unshift) => {
+    if (unshift) array.unshift(element);
+    else array.push(element);
+  };
 
   for (let i = 1; i < arr.length; i++) {
     for (let j = 0; j < i; j++) {
       if (arr[j] < arr[i]) {
-        const newLen = lengths[j] + 1;
-        if (newLen > lengths[i]) {
-          lengths[i] = newLen;
+        const newLen = counts[j] + 1;
+        if (newLen > counts[i]) {
+          counts[i] = newLen;
+          pushOrUnshift(validSequences[i], arr[j], decreasing)
         };
       };
     };
+
+    // add end node value to sequence
+    pushOrUnshift(validSequences[i], arr[i], decreasing)
   };
 
-  return Math.max(...lengths);
+  const highest = Math.max(...validSequences.map((list) => list.length));
+  if (highest < 3) return [];
+
+  const results = validSequences.filter((list) => list.length === highest);
+  console.log('results', results);
+
+  // reverse lists if looking for decreasing
+  return results.length > 1 ? results : results[0];
 };
