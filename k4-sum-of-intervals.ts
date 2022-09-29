71bfd6de37c4fb62648a4b9f71885582b290d743
symphony <jciurc@gmail.com>
2022-09-28 17:19:48 -0700

    try to sort method to compare next lows with previous highs

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -1,17 +1,9 @@
 const sumIntervals = (intervals) => {
   console.log('pre', intervals);
-  const offset = intervals.sort((a, b) => a[0] - b[0])[0][0];
-  const zeroAdjusted = intervals.map((val) => [val[0] - offset, val[1] - offset]);
+  const sorted = intervals.sort((a, b) => a[0] - b[1]);
+  console.log('sorted', sorted);
 
-  console.log('adjusted', zeroAdjusted);
-
-  const overlapping = [];
-
-  let sum = zeroAdjusted.reduce((prev, val) => {
-    if (val[0] <= prev) return val[1];
-    overlapping.push(val);
+  return sorted.reduce((sum, val) => {
+    return val[1] - val[0] - (val[0] - sum);
   }, 0)
-
-  console.log('remaining', overlapping);
-  return sum;
 };
