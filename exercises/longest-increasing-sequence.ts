0ba72fe176a92b4631c69d229e764af16c2f0562
symphony <jciurc@gmail.com>
2022-09-29 12:46:30 -0700

    refactor lis js tests

--- exercises/longest-increasing-sequence.ts
+++ exercises/longest-increasing-sequence.ts
@@ -14,32 +14,32 @@ function longestSequence(arr) {
   // is itself a minimum increasing subsequence.
   const lengthsArr = Array(arr.length).fill(1);
 
-  let prevElIdx = 0;
-  let currElIdx = 1;
+  let i = 0;
+  let j = 1;
 
-  while (currElIdx < arr.length) {
-    if (arr[prevElIdx] < arr[currElIdx]) {
+  while (j < arr.length) {
+    if (arr[i] < arr[j]) {
       // If current element is bigger then the previous one. then
       // it is a part of increasing subsequence where length is
       // by 1 bigger then the length of increasing subsequence
       // for the previous element.
-      const newLen = lengthsArr[prevElIdx] + 1;
-      if (newLen > lengthsArr[currElIdx]) {
+      const newLen = lengthsArr[i] + 1;
+      if (newLen > lengthsArr[j]) {
         // Increase only if previous element would give us a
         // bigger subsequence length then we already have for
         // current element.
-        lengthsArr[currElIdx] = newLen;
+        lengthsArr[j] = newLen;
       }
     }
 
     // Move previous element index right.
-    prevElIdx += 1;
+    i += 1;
 
     // If previous element index equals to current element index then
     // shift current element right and reset previous element index to zero.
-    if (prevElIdx === currElIdx) {
-      currElIdx += 1;
-      prevElIdx = 0;
+    if (i === j) {
+      j += 1;
+      i = 0;
     }
   }
 
@@ -56,28 +56,29 @@ function longestSequence(arr) {
   return longestIncreasingLength;
 }
 
-try {
-  assert.equal(longestSequence([1, 5, 2, 7, 3]), 3);
 
-  console.log('PASSED: ' + '`LISsolveDP([1,5,2,7,3])` should return `3`');
-} catch (err) {
-  console.log(err);
+try {
+  assert.equal(longestSequence([1, 5, 2, 7, 3]), 34);
+  console.log('PASSED');
+} catch {
+  console.log('-- FAILED --');
+} finally {
+  console.log('[1,5,2,7,3] should return 3\n');
 }
 
 try {
   assert.equal(longestSequence([13, 1, 3, 4, 8, 4]), 4);
-
-  console.log('PASSED: ' + '`LISsolveDP([13,1,3,4,8,4])` should return `4`');
-} catch (err) {
-  console.log(err);
+  console.log('PASSED');
+} catch {
+  console.log('-- FAILED --');
+} finally {
+  console.log('[13,1,3,4,8,4] should return 4\n');
 }
-
 try {
   assert.equal(longestSequence([13, 1, 3, 4, 8, 19, 17, 8, 0, 20, 14]), 6);
-
-  console.log(
-    'PASSED: ' + '`LISsolveDP([13,1,3,4,8,19,17,8,0,20,14])` should return `6`'
-  );
-} catch (err) {
-  console.log(err);
+  console.log('PASSED');
+} catch {
+  console.log('-- FAILED --');
+} finally {
+  console.log('[13,1,3,4,8,19,17,8,0,20,14] should return 6\n');
 }
