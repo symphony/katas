b94d84401679c5a439a12248f01f0da7f540fb3e
symphony <jciurc@gmail.com>
2022-09-28 17:05:22 -0700

    start new solution with reduce and an offset

--- k4-sum-of-intervals.ts
+++ k4-sum-of-intervals.ts
@@ -1,26 +1,9 @@
-const consolidate = (list) => {
-  const accumulator = [list.shift()];
-
-  for (const next of list) {
-    let overlap = false;
-
-    // problem code - quadratic algorithm
-    for (const prev of [...accumulator]) {
-      // check if fully above or below previous range
-      if (next[1] < prev[0] || next[0] > prev[1]) continue;
-
-      overlap = true;
-      // check for overlap and extend previous range start and end
-      if (next[0] < prev[0] && next[1] >= prev[0]) prev[0] = next[0];
-      if (next[1] > prev[1] && prev[0] <= prev[1]) prev[1] = next[1];
-    };
-    if (!overlap) accumulator.push(next);
-  };
-
-  // if lists are the same length it means no more consolidating can be done
-  return accumulator.length < list.length ? consolidate(accumulator) : accumulator;
-};
-
 const sumIntervals = (intervals) => {
-  return consolidate(intervals).reduce((prev, next) => prev += next[1] - next[0], 0);
+  const offset = intervals.sort((a, b) => a[0] - b[0])[0][0];
+  const zeroAdjust = intervals.map((val) => [val[0] - offset, val[1] - offset]);
+
+  const squashed = zeroAdjust.reduce((prev, val) => {
+    if (val[0] <= prev) return val[1];
+
+  }, 0)
 };
