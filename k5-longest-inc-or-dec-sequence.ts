675672a9d48bf00f64e0f51b6e4654c3e95e437f
symphony <jciurc@gmail.com>
2022-09-29 22:37:22 -0700

    refactor and clean up in LIS function

--- k5-longest-inc-or-dec-sequence.ts
+++ k5-longest-inc-or-dec-sequence.ts
@@ -6,15 +6,15 @@ const longestComb = (arr, command) => {
     const candidates = [];
     for (const sequence of validSequences) {
       if (compare(sequence[sequence.length - 1], end)) candidates.push([...sequence, end])
-      candidates.push(sequence)
+      candidates.push(sequence);
     };
-    candidates.push([end])
-    validSequences.splice(0, validSequences.length, ...candidates);
-  };
 
+    validSequences.splice(0, validSequences.length, ...candidates, [end]);
+  };
 
   const longest = Math.max(...validSequences.map((list) => list.length));
   const results = validSequences.filter((list) => list.length === longest);
   if (results[0].length < 3) return [];
-  return results.length === 1 ? results[0] : results;
+
+  return results.length > 1 ? results : results[0];
 };
