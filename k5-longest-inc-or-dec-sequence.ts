ebe5bb02d8a7a6c3839830bb0ca1a651315ec2cc
symphony <jciurc@gmail.com>
2022-09-29 22:06:15 -0700

    attempt shorte refactored LIS version

--- k5-longest-inc-or-dec-sequence.ts
+++ k5-longest-inc-or-dec-sequence.ts
@@ -1,35 +1,22 @@
-// = helpers =
-const filterLongest = (arr) => {
-  const longest = Math.max(...arr.map((list) => list.length));
-  return arr.filter((list) => list.length === longest);
-};
-const lookupSort = (left, right, external) => {
-  // find first pair with unique values
-  const i = left.findIndex((x, i) => external[external.indexOf(left[i])] !== external[external.indexOf(right[i])]);
-  const [a, b] = [external.indexOf(left[i]), external.indexOf(right[i])];
-  return a - b;
-};
-
-// = main function =
 const longestComb = (arr, command) => {
-  // convert all numbers to objects - necessary to distinguish duplicate values later
-  arr = arr.map((a) => [a]);
   const compare = (a, b) => command === '> >' || command === '>>' ? a > b : a < b;
-  const validSequences = [[arr[0]]];
-
-  for (let i = 1; i < arr.length; i++) {
-    const candidates = [];
-    for (const nums of validSequences) {
-      const [a, b] = [...nums[nums.length - 1], ...arr[i]];
-      if (compare(a, b)) candidates.push([...nums, arr[i]])
-    };
-
-    validSequences.push(...filterLongest(candidates[0] ? candidates : [[arr[i]]]));
+  const filterLongest = (array) => {
+    const longest = Math.max(...array.map((list) => list.length));
+    return array.filter((list) => list.length === longest);
   };
 
-  const results = filterLongest(validSequences);
+  const validSequences = [];
+  for (const end of arr) {
+    const candidates = [];
+    for (const sequence of validSequences) {
+      if (compare(sequence[sequence.length - 1], end)) candidates.push([...sequence, end])
+    };
+
+    validSequences.push(filterLongest(candidates));
+  };
+
+  const results = filterLongest(validSequences)
+
   if (results[0].length < 3) return [];
-  // convert sets back to integers
-  const parsed = results.sort((a, b) => lookupSort(a, b, arr)).map((list) => list.map((num) => num[0]));
-  return results.length === 1 ? parsed[0] : parsed;
+  return results.length === 1 ? results[0] : results;
 };
