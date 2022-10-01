45ae61b288d608b2e0ea21e18e9151f50bba6ca8
symphony <jciurc@gmail.com>
2022-10-01 15:47:50 -0700

    refactor boss is right

--- k6-boss-is-right.ts
+++ k6-boss-is-right.ts
@@ -1,11 +1,9 @@
 function correctOrder(list) {
   return list.map(({ tire, steeringWheel, totalPrice }) => {
-    for (let tireCost = 5.98; tireCost < 19.79; tireCost++) {
-      const wheelCost = (totalPrice - tireCost * tire) / steeringWheel;
-      if (wheelCost < tireCost || wheelCost > tireCost + 3) continue;
-      if (wheelCost.toFixed(2).endsWith('.79')) {
-        const price = Math.round((tireCost * 4 + wheelCost) * 100) / 100;
-        return { tire: 4, steeringWheel: 1, totalPrice: price };
+    for (let tires = 5.98; tires < 19.79; tires++) {
+      for (let wheels = tires + 0.81; wheels < tires + 3; wheels++) {
+        if ((tires * tire + wheels * steeringWheel).toFixed(2) == totalPrice)
+          return { tire: 4, steeringWheel: 1, totalPrice: +(tires * 4 + wheels).toFixed(2) };
       };
     };
   });
