e7625e39698e377b5177ecebbd756968eb3400a7
symphony <jciurc@gmail.com>
2022-10-01 15:09:22 -0700

    refactor boss is right to round only where needed

--- k6-boss-is-right.ts
+++ k6-boss-is-right.ts
@@ -1,10 +1,10 @@
 function correctOrder(list) {
   return list.map(({ tire, steeringWheel, totalPrice }) => {
     for (let tireCost = 5.98; tireCost < 19.79; tireCost++) {
-      const wheelCost = Math.round(((totalPrice - (tireCost * tire)) / steeringWheel) * 100) / 100;;
+      const wheelCost = (totalPrice - tireCost * tire) / steeringWheel;
       if (wheelCost < tireCost || wheelCost > tireCost + 3 || wheelCost > 20) continue;
-      if (Math.round(wheelCost * 100) % 100 === 79) {
-        const price = Math.round(((tireCost * 4) + wheelCost) * 100) / 100;
+      if (wheelCost.toFixed(2).endsWith('.79')) {
+        const price = Math.round((tireCost * 4 + wheelCost) * 100) / 100;
         return { tire: 4, steeringWheel: 1, totalPrice: price };
       };
     };
