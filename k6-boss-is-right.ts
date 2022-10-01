0f885ad7ddc159491f8f6c255fc2cd2242545724
symphony <jciurc@gmail.com>
2022-10-01 15:13:21 -0700

    remove 3rd conditional (cost above 20) as a solution will be found before it reaches

--- k6-boss-is-right.ts
+++ k6-boss-is-right.ts
@@ -2,7 +2,7 @@ function correctOrder(list) {
   return list.map(({ tire, steeringWheel, totalPrice }) => {
     for (let tireCost = 5.98; tireCost < 19.79; tireCost++) {
       const wheelCost = (totalPrice - tireCost * tire) / steeringWheel;
-      if (wheelCost < tireCost || wheelCost > tireCost + 3 || wheelCost > 20) continue;
+      if (wheelCost < tireCost || wheelCost > tireCost + 3) continue;
       if (wheelCost.toFixed(2).endsWith('.79')) {
         const price = Math.round((tireCost * 4 + wheelCost) * 100) / 100;
         return { tire: 4, steeringWheel: 1, totalPrice: price };
