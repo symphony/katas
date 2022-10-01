7964105f8f3d7837092f653f3cbbd93e6689c738
symphony <jciurc@gmail.com>
2022-10-01 12:13:25 -0700

    use array for ship lookup

--- k4-battleship-validator.ts
+++ k4-battleship-validator.ts
@@ -1,12 +1,5 @@
 function validateBattlefield(field) {
-  // {[ship length]: ship count}
-  const ships = {
-    '4': 1, // battleship
-    '3': 2, // cruiser
-    '2': 3, // destroyer
-    '1': 4, // submarine
-  };
-  // build map to keep track of visited coordinates
+  const ships = [0, 4, 3, 2, 1]
   const marked = field.map((row) => row.map((col) => false));
 
   /** checks forward neighbouring coords for ships - returns false if invalid ship found   */
@@ -29,12 +22,11 @@ function validateBattlefield(field) {
   for (let y = 0; y < field.length; y++) {
     for (let x = 0; x < field[y].length; x++) {
       const found = checkForShips(y, x, 'both');
-      if (found === 0) continue;
       if (found === false || found > 4) return false;
       ships[found]--;
     };
   };
 
   // false for any value that isn't 0
-  return !Object.values(ships).some(Boolean);
+  return !ships.slice(1).some(Boolean);
 };
