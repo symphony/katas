ec1e70bf924348defdf993d4b3374cca397214f7
symphony <jciurc@gmail.com>
2022-10-01 14:53:25 -0700

    add rounding to return value

--- k6-boss-is-right.ts
+++ k6-boss-is-right.ts
@@ -1,15 +1,12 @@
 function correctOrder(list) {
-  console.log(list.length);
   return list.map(({ tire, steeringWheel, totalPrice }) => {
     for (let tireCost = 5.98; tireCost < 19.79; tireCost++) {
       const wheelCost = Math.round(((totalPrice - (tireCost * tire)) / steeringWheel) * 100) / 100;;
       if (wheelCost < tireCost || wheelCost > tireCost + 3 || wheelCost > 20) continue;
-      console.log(tireCost, wheelCost);
       if (Math.round(wheelCost * 100) % 100 === 79) {
-        return { tire: 4, steeringWheel: 1, totalPrice: (tireCost * 4) + wheelCost };
+        const price = Math.round(((tireCost * 4) + wheelCost) * 100) / 100;
+        return { tire: 4, steeringWheel: 1, totalPrice: price };
       }
-      console.log(tireCost, wheelCost,wheelCost * 100, wheelCost * 100 % 100,(wheelCost * 100 % 100 === 79));
     };
-    console.log('didn\' find anything\n\n\n');
   });
 };
