b956cc2514ac1a811886ffc60710c676a6b13196
symphony <jciurc@gmail.com>
2022-10-01 11:20:20 -0700

    add more conditions to battleship validator

--- k4-battleship-validator.ts
+++ k4-battleship-validator.ts
@@ -1,16 +1,10 @@
 function validateBattlefield(field) {
-  const lengths = {
-    4: 'battleship',
-    3: 'cruiser',
-    2: 'destroyer',
-    1: 'submarine',
-  };
-
+  // {length: count}
   const ships = {
-    battleship: 4,
-    cruiser: 2,
-    destroyer: 2,
-    submarine: 4,
+    4: 4,
+    3: 2,
+    2: 2,
+    1: 4,
   };
 
   // build map to keep track of visited coordinates
@@ -18,24 +12,32 @@ function validateBattlefield(field) {
 
   /** checks forward neighbouring coords for ships - returns false if invalid ship found   */
   const checkForShips = (y, x) => {
+    console.log('checking', y, x, marked[y][x], field[y][x]);
     if (marked[y][x]) return 0; // skip
     marked[y][x] = true;
 
     if (!field[y][x]) return 0;
-    if (field[y - 1][x + 1] || field[y + 1][x + 1]) return false;
-    return 1 + checkForShips(y, x + 1) + checkForShips(y + 1, x);
+    if (field[y - 1]?.[x + 1] || field[y + 1]?.[x + 1]) return false;
+
+    const right = checkForShips(y, x + 1);
+    const down = checkForShips(y + 1, x)
+    if (right === false || down === false) return false;
+    if (right && down) return false;
+
+    return 1 + right + down;
   };
 
   for (let y = 0; y < field.length; y++) {
     for (let x = 0; x < field[y].length; x++) {
-
       const found = checkForShips(y, x);
-      // false = invalid
+      if (found === 0) continue;
       if (found === false || found > 4) return false;
-      if (found > 0) ships[lengths[found]]--;
-
-      // ensure all ship counts are 0
-      return !Object.values(ships).every(Boolean);
+      console.log('found', found);
+      console.log('count', ships[found] - 1);
+      if (--ships[found] < 0) return false;
     };
+    console.log(ships, !Object.values(ships).some(Boolean));
   };
+
+  return true;
 };
