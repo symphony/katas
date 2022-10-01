a1b4c799fa6673ad0eef1f0c928b9eaa19e5f141
symphony <jciurc@gmail.com>
2022-10-01 15:49:59 -0700

    add edge case for wheels > 20 possibilty

--- k6-boss-is-right.ts
+++ k6-boss-is-right.ts
@@ -1,7 +1,7 @@
 function correctOrder(list) {
   return list.map(({ tire, steeringWheel, totalPrice }) => {
-    for (let tires = 5.98; tires < 19.79; tires++) {
-      for (let wheels = tires + 0.81; wheels < tires + 3; wheels++) {
+    for (let tires = 5.98; tires < 20; tires++) {
+      for (let wheels = tires + 0.81; wheels < Math.min(tires + 3, 20); wheels++) {
         if ((tires * tire + wheels * steeringWheel).toFixed(2) == totalPrice)
           return { tire: 4, steeringWheel: 1, totalPrice: +(tires * 4 + wheels).toFixed(2) };
       };
