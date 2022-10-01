595b172731c67fc021e4a82bbf7e9cde37351e21
symphony <jciurc@gmail.com>
2022-10-01 12:05:48 -0700

    refactor and mark up battleship validator

--- k4-battleship-validator.ts
+++ k4-battleship-validator.ts
@@ -11,31 +11,30 @@ function validateBattlefield(field) {
 
   /** checks forward neighbouring coords for ships - returns false if invalid ship found   */
   const checkForShips = (y, x, dir) => {
-    if (marked[y][x]) return 0; // skip
+    if (marked[y][x]) return 0; // skip if already checked
     marked[y][x] = true;
 
+    // check corners
     if (!field[y][x]) return 0;
     if (field[y - 1]?.[x + 1] || field[y + 1]?.[x + 1]) return false;
 
+    // check adjacent
     const right = !(dir === 'down') ? checkForShips(y, x + 1, 'right') : 0;
-    if (right === false) return false;
-
     const down = !(dir === 'right') ? checkForShips(y + 1, x, 'down') : 0;
-    if (down === false) return false;
-    if (right && down) return false;
+    if (right === false || down === false) return false;
 
     return 1 + right + down;
   };
 
   for (let y = 0; y < field.length; y++) {
     for (let x = 0; x < field[y].length; x++) {
-      const found = checkForShips(y, x);
+      const found = checkForShips(y, x, 'both');
       if (found === 0) continue;
       if (found === false || found > 4) return false;
       ships[found]--;
     };
   };
 
+  // false for any value that isn't 0
   return !Object.values(ships).some(Boolean);
 };
-
