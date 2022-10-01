5f05ccfbbb9eeac1a70be733ec1a03af76dac5f5
symphony <jciurc@gmail.com>
2022-10-01 15:54:01 -0700

    shorten boss is right

--- k6-boss-is-right.ts
+++ k6-boss-is-right.ts
@@ -1,10 +1,8 @@
 function correctOrder(list) {
   return list.map(({ tire, steeringWheel, totalPrice }) => {
-    for (let tires = 5.98; tires < 20; tires++) {
-      for (let wheels = tires + 0.81; wheels < Math.min(tires + 3, 20); wheels++) {
-        if ((tires * tire + wheels * steeringWheel).toFixed(2) == totalPrice)
-          return { tire: 4, steeringWheel: 1, totalPrice: +(tires * 4 + wheels).toFixed(2) };
-      };
-    };
+    for (let tires = 5.98; tires < 20; tires++)
+      for (let wheel = tires + 0.81; wheel < Math.min(tires + 3, 20); wheel++)
+        if ((tire * tires + steeringWheel * wheel).toFixed(2) == totalPrice)
+          return { tire: 4, steeringWheel: 1, totalPrice: +(tires * 4 + wheel).toFixed(2) };
   });
 };
