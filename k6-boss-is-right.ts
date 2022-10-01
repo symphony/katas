f355aa768d524be0d21dc8154ff21aa2f884be08
symphony <jciurc@gmail.com>
2022-10-01 14:49:10 -0700

    update boss is right kata. contains rounding bug

--- k6-boss-is-right.ts
+++ k6-boss-is-right.ts
@@ -1,5 +1,15 @@
-function correctOrder(list){
-  return list.map(({tire, steeringWheel, totalPrice}) => {
-
+function correctOrder(list) {
+  console.log(list.length);
+  return list.map(({ tire, steeringWheel, totalPrice }) => {
+    for (let tireCost = 5.98; tireCost < 19.79; tireCost++) {
+      const wheelCost = Math.round(((totalPrice - (tireCost * tire)) / steeringWheel) * 100) / 100;;
+      if (wheelCost < tireCost || wheelCost > tireCost + 3 || wheelCost > 20) continue;
+      console.log(tireCost, wheelCost);
+      if (Math.round(wheelCost * 100) % 100 === 79) {
+        return { tire: 4, steeringWheel: 1, totalPrice: (tireCost * 4) + wheelCost };
+      }
+      console.log(tireCost, wheelCost,wheelCost * 100, wheelCost * 100 % 100,(wheelCost * 100 % 100 === 79));
+    };
+    console.log('didn\' find anything\n\n\n');
   });
 };
