47625a4baa1c1b38f458c6ba45eaa38ada8361cf
symphony <jciurc@gmail.com>
2022-10-01 20:13:08 -0700

    finish gap in primes

--- k5-gap-in-primes.ts
+++ k5-gap-in-primes.ts
@@ -1,38 +1,24 @@
-/**
- * The prime numbers are not regularly spaced. For example from 2 to 3 the gap is 1. From 3 to 5 the gap is 2. From 7 to 11 it is 4. Between 2 and 50 we have the following pairs of 2-gaps primes: 3-5, 5-7, 11-13, 17-19, 29-31, 41-43
- *
- * A prime gap of length n is a run of n-1 consecutive composite numbers between two successive primes (see: http://mathworld.wolfram.com/PrimeGaps.html).
- *
- *
- * @param g
- * @param m
- * @param n
- */
-const gap = (g, m, n) => {
-  const isPrime = (n) => {
-    if (n <= 1) return false;
-    if (n === 2) return true;
-
+function gap(g, m, n) {
+  const isComposite = (n) => {
+    if (n <= 2) return false;
     for (let i = 2; i < n; i++)
-      if (n % i === 0) return true;
-
-    return false;
+      if (n % i === 0) return false;
+    return true;
   };
 
-
-  let start = m;
   for (let i = m; m <= n; i++) {
-    for (let j = 2; j < i; j++) {
-      if (!isPrime(i)) {
-        start = i;
-        const end = ++i + g;
-        if (end > n) return null;
+    if (n < m + g) return null;
 
-        for (i; i < end; i++) {
-          if (isPrime(i)) break;
-          return [start, i + 1];
-        };
-      };
+    if (isComposite(i)) {
+      const start = i;
+      const end = i++ + g;
+      if (end > n) return null;
+      if (!isComposite(end)) continue;
+
+      const range = Array.from([...Array(g - 1).keys()], (k) => k += i);
+      const found = range.find(isComposite);
+      if (!found) return [start, end];
+      i = found - 1;
     };
   };
 
