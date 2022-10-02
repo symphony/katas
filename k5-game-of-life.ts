499c602dfa59a474094952489948e45449cc436e
symphony <jciurc@gmail.com>
2022-10-01 23:09:24 -0700

    update game of life. set up loops and results structure

--- k5-game-of-life.ts
+++ k5-game-of-life.ts
@@ -1,6 +1,16 @@
 function nextGen(cells) {
-  const action = (n, alive) => (alive && n === 2 || n === 3) ? 1 : 0;
-  const next = [[]];
+  const state = (n, alive) => (alive && n === 2 || n === 3) ? 1 : 0;
+  const count = cells.map((a) => a.map((b) => 0));
 
-  return [[]];
+  for (let r = 0; r < cells.length; r++) {
+    for (let c = 0; c < cells[r].length; c++) {
+      for (const y of [-1, 1]) {
+        for (const x of [-1, 0, 1]) {
+          count[r][c] += cells[r + y]?.[c + x] || 0;
+        };
+      };
+    };
+  };
+
+  return count.map((cell) => cell.map(state));
 };
