f7b7c59b896fc9e73a6820f4e5b213cd35762da2
symphony <jciurc@gmail.com>
2022-10-02 00:17:13 -0700

    refactor game of life to use map and reduce

--- k5-game-of-life.ts
+++ k5-game-of-life.ts
@@ -1,19 +1,11 @@
 function nextGen(cells) {
-  const state = ({ alive, friends }) => (alive && friends === 2 || friends === 3) ? 1 : 0;
-  const data = cells.map((a) => a.map((b) => ({ alive: false, friends: 0 })));
+  const state = (alive, friends) => (alive && friends === 2 || friends === 3) ? 1 : 0;
 
-  for (let y = 0; y < cells.length; y++) {
-    for (let x = 0; x < cells[y].length; x++) {
-      for (const a of [-1, 0, 1]) {
-        data[y][x].alive = Boolean(cells[y][x]);
-
-        for (const b of [-1, 0, 1]) {
-          if (a === 0 && b === 0) continue;
-          data[y][x].friends += cells[y + a]?.[x + b] || 0;
-        };
-      };
-    };
-  };
-
-  return data.map((cell) => cell.map(state));
+  return cells.map((row, i) => (
+    row.map((cell, j) => (
+      state(cell, [-1, 0, 1]
+        .reduce((friends, x) => friends + [-1, 0, 1]
+          .reduce((sum, y) => (
+            x || y ? (cells[i + y]?.[j + x] || 0) + sum : sum), 0), 0)
+      )))));
 };
