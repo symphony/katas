919832283fd40a262044356c399b65ce6ceb679a
symphony <jciurc@gmail.com>
2022-10-02 00:26:07 -0700

    shorten game of life. clean up style

--- k5-game-of-life.ts
+++ k5-game-of-life.ts
@@ -1,11 +1,10 @@
-function nextGen(cells) {
-  const state = (alive, friends) => (alive && friends === 2 || friends === 3) ? 1 : 0;
-
-  return cells.map((row, i) => (
+const state = (alive, friends) => (alive && friends === 2 || friends === 3) ? 1 : 0;
+const nextGen = (cells) => (
+  cells.map((row, i) => (
     row.map((cell, j) => (
       state(cell, [-1, 0, 1]
         .reduce((friends, x) => friends + [-1, 0, 1]
           .reduce((sum, y) => (
             x || y ? (cells[i + y]?.[j + x] || 0) + sum : sum), 0), 0)
-      )))));
-};
+      )))))
+);
