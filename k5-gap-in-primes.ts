fab7adec1881d212d6e6e920d5beb0c32d9a442c
symphony <jciurc@gmail.com>
2022-10-01 20:46:02 -0700

    simplify gap in primes function. use simple difference comparison instead of looping

--- k5-gap-in-primes.ts
+++ k5-gap-in-primes.ts
@@ -1,24 +1,19 @@
 function gap(g, m, n) {
-  const isComposite = (n) => {
-    if (n <= 2) return false;
+  const isPrime = (n) => {
+    if (n <= 1) return false;
+    if (n === 2) return true;
     for (let i = 2; i * i <= n; i++)
-      if (n % i === 0) return false;
-    return true;
+      if (n % i === 0) return true;
+    return false;
   };
 
+  let start = null;
   for (let i = m; i <= n; i++) {
     if (n < m + g) return null;
 
-    if (isComposite(i)) {
-      const start = i;
-      const end = i++ + g;
-      if (end > n) return null;
-      if (!isComposite(end)) continue;
-
-      const range = Array.from([...Array(g - 1).keys()], (k) => k += i);
-      const found = range.find(isComposite);
-      if (!found) return [start, end];
-      i = found - 1;
+    if (!isPrime(i)) {
+      if (i - start === g) return [start, i];
+      start = i;
     };
   };
 
