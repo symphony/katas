77eb5f810aa18bee8813e84a7e47ffd81873385e
symphony <jciurc@gmail.com>
2022-10-07 21:04:23 -0700

    solve findDuplicates in o(n) using set and flatMap

--- find-duplicates.ts
+++ find-duplicates.ts
@@ -1,20 +1,17 @@
 function findDuplicateIndexes(arr) {
-  const map = {}
+  const map = {};
   arr.forEach((item, i) => { map[item] = i });
 
-  const duplicateIndexes = new Set();
-  arr.forEach((item, i) => {
-    if (i !== map[item]) duplicateIndexes.add(map[item]).add(i);
-  });
-
-  return [...duplicateIndexes];
+  return [... new Set(arr.flatMap((item, i) => (
+    i !== map[item] ? [i, map[item]] : [])
+  ))];
 };
 
 const arr1 = ['banana', 'apple', 'blueberry', 'apple', 'orange'];
 const arr2 = ['banana', 'apple', 'blueberry', 'apple', 'apple'];
 
 console.log(findDuplicateIndexes(arr1));
-// return [1, 3]
+// returns [1, 3]
 
 console.log(findDuplicateIndexes(arr2));
 // returns [1, 3, 4]
