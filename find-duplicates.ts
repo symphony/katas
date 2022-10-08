ff42bf66229641cb4b5e0491b7072df3377d9519
symphony <jciurc@gmail.com>
2022-10-07 21:38:19 -0700

    update test case findduplicates. revert back to set and flatmap solution

--- find-duplicates.ts
+++ find-duplicates.ts
@@ -1,6 +1,5 @@
 function findDuplicateIndexes(arr) {
-  const map = {};
-  arr.forEach((item, i) => { map[item] = i });
+  const map = Object.assign({}, ...arr.map((item, i) => ({ [item]: i })));
 
   return [... new Set(arr.flatMap((item, i) => (
     i !== map[item] ? [i, map[item]] : [])
@@ -8,7 +7,7 @@ function findDuplicateIndexes(arr) {
 };
 
 const arr1 = ['banana', 'apple', 'blueberry', 'apple', 'orange'];
-const arr2 = ['banana', 'apple', 'blueberry', 'apple', 'apple'];
+const arr2 = ['banana', 'apple', 'blueberry', 'apple', 'apple', 'blueberry', 'apple',];
 
 console.log(findDuplicateIndexes(arr1));
 // returns [1, 3]
