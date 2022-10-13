3d346d4d8e1cb7e21f76c54132533d89ca58607d
symphony <jciurc@gmail.com>
2022-10-12 22:35:09 -0700

    update range extractor

--- k4-range-extractor.ts
+++ k4-range-extractor.ts
@@ -1,3 +1,15 @@
 function solution(list) {
-  // TODO: complete solution
+  return list.reduce((acc, val, i, arr) => {
+    if (val === arr[i + 2] - 2) {
+      for (let j = i + 3; j < arr.length; j++) {
+        if (arr[j] !== val + j) {
+          arr.splice(i + 1, j, `${val}-${val + j - i}`);
+          return acc;
+        };
+      };
+
+    };
+    acc.push(val);
+    return acc;
+  }, []).join();
 };
