f31450a31959767ccfea22ea6fff633d400b31dc
symphony <jciurc@gmail.com>
2022-10-12 23:01:39 -0700

    update k3 smaller

--- k3-smaller.py
+++ k3-smaller.py
@@ -1,24 +1,63 @@
 def smaller(arr):
-    res = []
-    sorted = []
-    def addnum(num):
-        if len(sorted) == 0 or num > sorted[-1]:
-            sorted.append(num)
-            return len(sorted) - 1
-        if num <= sorted[0]:
-            sorted.insert(0,num)
-            return 0
-        left = 0
-        right = len(sorted) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if sorted[mid + 1]  < num:
-                left = mid + 1
-            if sorted[mid] >= num:
-                right = mid
-            if sorted[mid] < num and sorted[mid + 1] >= num:
-                sorted.insert(mid + 1,num)
-                return mid + 1
-    for i in range(len(arr) - 1, -1 ,-1):
-        res.insert(0,addnum(arr[i]))
-    return res
+
+    return
+
+
+
+
+const longestComb = (arr, command) => {
+  const compare = (a, b) => command === '> >' || command === '>>' ? a > b : a < b;
+  const validSequences = [];
+
+  for (const end of arr) {
+    const candidates = [];
+    for (const sequence of validSequences) {
+      if (compare(sequence[sequence.length - 1], end)) candidates.push([...sequence, end]);
+      candidates.push(sequence);
+    };
+
+    validSequences.splice(0, validSequences.length, ...candidates, [end]);
+  };
+
+  const longest = Math.max(...validSequences.map((list) => list.length));
+  const results = validSequences.filter((list) => list.length === longest);
+  if (results[0].length < 3) return [];
+
+  return results.length > 1 ? results : results[0];
+};
+
+
+
+
+
+
+
+
+
+
+
+
+# def smaller(arr):
+#     res = []
+#     sorted = []
+#     def addnum(num):
+#         if len(sorted) == 0 or num > sorted[-1]:
+#             sorted.append(num)
+#             return len(sorted) - 1
+#         if num <= sorted[0]:
+#             sorted.insert(0,num)
+#             return 0
+#         left = 0
+#         right = len(sorted) - 1
+#         while left <= right:
+#             mid = (left + right) // 2
+#             if sorted[mid + 1]  < num:
+#                 left = mid + 1
+#             if sorted[mid] >= num:
+#                 right = mid
+#             if sorted[mid] < num and sorted[mid + 1] >= num:
+#                 sorted.insert(mid + 1,num)
+#                 return mid + 1
+#     for i in range(len(arr) - 1, -1 ,-1):
+#         res.insert(0,addnum(arr[i]))
+#     return res
--- k3-smaller.ts
+++ k3-smaller.ts
@@ -1,3 +1,16 @@
 function smaller(arr) {
-  const sorted = arr;
+  const count = new Map(arr.map((num) => [num, []]));
+  const countSmaller = (n, rest) => {
+    for (let i = 0; i < rest.lengthl i++) {
+      if (rest[i] < n) {
+        count.get(n.push(num));
+      } else {
+
+      };
+    };
+  };
+
+
+
+  console.log();
 };
