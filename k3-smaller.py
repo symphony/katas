3021d6b5be85b301dc4d7c8e077c04ec0d4a2570
symphony <jciurc@gmail.com>
2022-10-13 17:05:24 -0700

    tried comparing next index in smaller. didn't work

--- k3-smaller.py
+++ k3-smaller.py
@@ -3,40 +3,6 @@ def smaller(arr):
     return
 
 
-
-
-const longestComb = (arr, command) => {
-  const compare = (a, b) => command === '> >' || command === '>>' ? a > b : a < b;
-  const validSequences = [];
-
-  for (const end of arr) {
-    const candidates = [];
-    for (const sequence of validSequences) {
-      if (compare(sequence[sequence.length - 1], end)) candidates.push([...sequence, end]);
-      candidates.push(sequence);
-    };
-
-    validSequences.splice(0, validSequences.length, ...candidates, [end]);
-  };
-
-  const longest = Math.max(...validSequences.map((list) => list.length));
-  const results = validSequences.filter((list) => list.length === longest);
-  if (results[0].length < 3) return [];
-
-  return results.length > 1 ? results : results[0];
-};
-
-
-
-
-
-
-
-
-
-
-
-
 # def smaller(arr):
 #     res = []
 #     sorted = []
--- k3-smaller.ts
+++ k3-smaller.ts
@@ -1,16 +1,39 @@
 function smaller(arr) {
-  const count = new Map(arr.map((num) => [num, []]));
-  const countSmaller = (n, rest) => {
-    for (let i = 0; i < rest.lengthl i++) {
-      if (rest[i] < n) {
-        count.get(n.push(num));
-      } else {
+  const counts = arr.map((n) => ({ public: 0, personal: 0 }));
+  const outliers = [];
+  outliers[arr.length - 1] = arr[arr.length - 1];
 
-      };
+  const compare = (l, r) => {
+    if (arr[l] > arr[r]) {
+      counts[l].personal = counts[r].public + 1;
+      counts[l].public = counts[r].public + 1;
+    } else if (arr[l] === arr[r]) {
+      counts[l].personal = counts[r].personal;
+      counts[l].public = counts[r].public + 1;
+      counts[r].public = 0;
     };
   };
 
+  for (let l = arr.length - 2, r = l + 1; l >= 0; l--, r--) {
+    compare(l, r);
+    if (arr[l] < arr[r]) {
+      const found = outliers.findIndex((n) => (arr[l] >= n));
+      if (found !== -1) compare(l, found);
+      else outliers[l] = arr[l];
+    };
+  };
 
-
-  console.log();
+  console.log(counts);
+  return counts.map((count) => count.personal);
 };
+
+
+
+
+// assert.deepEqual(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0]);
+// assert.deepEqual(smaller([1, 2, 3]), [0, 0, 0]);
+// assert.deepEqual(smaller([1, 2, 0]), [1, 1, 0]);
+// assert.deepEqual(smaller([1, 2, 1]), [0, 1, 0]);
+// assert.deepEqual(smaller([1, 1, -1, 0, 0]), [3, 3, 0, 0, 0]);
+// assert.deepEqual(smaller([1, 3, -1, 2, -3]), [2, 3, 1, 1, 0]);
+// assert.deepEqual(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6]), [4, 1, 5, 5, 0, 0, 0, 0, 0]);
