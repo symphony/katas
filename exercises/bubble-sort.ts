4cbfad6eac9f0fc6e09e45088bf4f53047f18e00
symphony <jciurc@gmail.com>
2022-10-14 17:38:58 -0700

    convert recursive sort to while loop

--- exercises/bubble-sort.ts
+++ exercises/bubble-sort.ts
@@ -1,20 +1,26 @@
-const bubbleSort = (list, finished = true) => {
-  for (let i = 0; i < list.length - 1; i++) {
-    const [a, b] = [list[i], list[i + 1]];
-    if (b < a) {
-      list.splice(i, 2, b, a)
-      finished = false;
-    };
+const bubbleSort = (list) => {
+  let lastEle = list.length - 1;
+  let finished = false;
 
+  while (!finished) {
+    finished = true;
+
+    for (let i = 0; i < lastEle; i++) {
+      const [a, b] = [list[i], list[i + 1]];
+      if (b < a) {
+        list.splice(i, 2, b, a);
+        finished = false;
+      };
+    };
   };
 
-  return finished ? list : bubbleSort(list);
+  return list;
 };
 
 
 const array1 = [4, 6, 1, 4, -1, 7, 3,];
 const array2 = [5, 5, 5, 3, 2, 1, 20, 320, -5];
-const array3 = ['potato', 'pickle', 'apple','potato'];
+const array3 = ['potato', 'pickle', 'apple', 'potato'];
 
 console.log(bubbleSort(array1));
 // -1, 1, 3, 4, 4, 6, 7
