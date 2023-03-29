bd35733e819dd40da03649d1ebcadf4e7cb86080
symphony <jciurc@gmail.com>
2023-03-28 22:35:47 -0700

    add regex check for symbols in simple pig latin

--- k5-simple-pig-latin.ts
+++ k5-simple-pig-latin.ts
@@ -1,4 +1,4 @@
 export const pigIt = (a: string): string => {
   const words = a.split(' ');
-  return words.map((word: string) => `${word.slice(1)}${word[0]}ay`).join(' ');
+  return words.map((word: string) => /[a-zA-Z]/.test(word) ? `${word.slice(1)}${word[0]}ay` : word).join(' ');
 };
