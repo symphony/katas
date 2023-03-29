14fcc6939f1970b648ecdba6c9b2f421e520f369
symphony <jciurc@gmail.com>
2023-03-28 22:24:11 -0700

    ignore case in duplicate encoder

--- k6-duplicate-encoder.ts
+++ k6-duplicate-encoder.ts
@@ -1,9 +1,9 @@
 export function duplicateEncode(word: string) {
   const table: { [key: string]: string; } = {};
 
-  for (const k of word.split('')) {
+  for (const k of word.toLowerCase().split('')) {
     table[k] = table[k] ? ')' : '(';
   }
 
-  return word.split('').map((k: string) => table[k]).join('');
+  return word.toLowerCase().split('').map((k: string) => table[k]).join('');
 }
