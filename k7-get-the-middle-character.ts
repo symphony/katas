9e4bd0bf2579ecadb47d32faf685992344da16be
symphony <jciurc@gmail.com>
2023-03-29 00:56:54 -0700

    fix get middle character

--- k7-get-the-middle-character.ts
+++ k7-get-the-middle-character.ts
@@ -1,5 +1,5 @@
 export function getMiddle(s: string) {
-  const i = Math.floor(s.length / 2);
+  const i = Math.floor(s.length / 2) + 1;
   const isEven = s.length % 2 === 0;
-  return isEven ? s.slice(i - 1, i + 2) : s.slice(i, i + 1);
+  return isEven ? s.slice(i - 2, i) : s.slice(i - 1, i);
 }
