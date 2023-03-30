530c4ec184754bb4a78c29da1d2bbecad3931b8d
symphony <jciurc@gmail.com>
2023-03-29 22:00:13 -0700

    new k7 chain adding function

---
+++ k5-chain-adding-function.ts
@@ -0,0 +1,3 @@
+export default function add(x: number): any {
+  // Curry away, curry away, curry away me hearties!
+}
--- k7-get-the-middle-character.ts
+++ k7-get-the-middle-character.ts
@@ -1,5 +1,5 @@
 export function getMiddle(s: string) {
-  const i = Math.floor(s.length / 2) + 1;
+  const i = Math.ceil(s.length / 2);
   const isEven = s.length % 2 === 0;
   return isEven ? s.slice(i - 2, i) : s.slice(i - 1, i);
 }
