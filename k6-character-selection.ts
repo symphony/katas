3bb6278dfbb52490745beb880d31784cd90cabff
symphony <jciurc@gmail.com>
2023-03-31 19:46:09 -0700

    use map instead of for of in character selection

--- k6-character-selection.ts
+++ k6-character-selection.ts
@@ -3,15 +3,13 @@ type Move = 'down' | 'up' | 'right' | 'left';
 export function streetFighterSelection(fighters: string[][], position: number[], moves: Move[]) {
   let y = position[0];
   let x = position[1];
-  const visited: string[] = [];
 
-  for (const move of moves) {
+  return moves.map((move) => {
     if (move === 'up') y = Math.max(0, y - 1);
     if (move === 'down') y = Math.min(1, y + 1);
     if (move === 'left') x = (x - 1 + fighters[y].length) % fighters[y].length;
     if (move === 'right') x = (x + 1 + fighters[y].length) % fighters[y].length;
-    visited.push(fighters[y][x]);
-  }
 
-  return visited;
+    return fighters[y][x];
+  });
 };
