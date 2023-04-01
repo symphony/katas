7574b42bd16f2d5874d3cc0c2aca7172edc77881
symphony <jciurc@gmail.com>
2023-03-31 19:44:43 -0700

    finish character selection

--- k6-character-selection.ts
+++ k6-character-selection.ts
@@ -1,5 +1,17 @@
 type Move = 'down' | 'up' | 'right' | 'left';
 
 export function streetFighterSelection(fighters: string[][], position: number[], moves: Move[]) {
-  // Your code here!
-}
+  let y = position[0];
+  let x = position[1];
+  const visited: string[] = [];
+
+  for (const move of moves) {
+    if (move === 'up') y = Math.max(0, y - 1);
+    if (move === 'down') y = Math.min(1, y + 1);
+    if (move === 'left') x = (x - 1 + fighters[y].length) % fighters[y].length;
+    if (move === 'right') x = (x + 1 + fighters[y].length) % fighters[y].length;
+    visited.push(fighters[y][x]);
+  }
+
+  return visited;
+};
